fce114ad3afb0f5f103e38dd1a837779f5786f9f
Tirach <jmelton098@gmail.com>
2022-01-22 12:18:53 +0800

    add post game release

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,34 @@
 const countdown = () => {
-  // Find a way to get this date to be converted to UTC-05:00
-  // OR
-  // Just add or minus the difference in the time below
-  const countDate = new Date("January 21, 2022 00:00:00").getTime();
+  const countDate = new Date("February 25, 2022 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
-  // 1000 milliseconds = 1 second
-  const second = 1000;
-  // 1 second times 60
-  const minute = second * 60;
-  // 60 minutes times 60
-  const hour = minute * 60;
-  // 60 minutes times 24
-  const day = hour * 24;
+  if (now >= countDate) {
+    document.querySelector(".dynaH2").innerText =
+      "Elden Ring has been on shelves for";
+  } else {
+    // 1000 milliseconds = 1 second
+    const second = 1000;
+    // 1 second times 60
+    const minute = second * 60;
+    // 60 minutes times 60
+    const hour = minute * 60;
+    // 60 minutes times 24
+    const day = hour * 24;
 
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
 
-  document.querySelector(".day").innerText = textDay;
-  document.querySelector(".hour").innerText = textHour;
-  document.querySelector(".minute").innerText = textMinute;
-  document.querySelector(".second").innerText = textSecond;
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+  }
+
+  setInterval(countdown, 1000);
 };
-
-setInterval(countdown, 1000);
 
 var totalImages = 19;
 
